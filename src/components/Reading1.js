a449e7e22d83401e659efc425e35e43750dcf0d2
TTS Pham Toan Phuc <kiaisoft.phuc.pham@gmail.com>
2023-06-13 21:08:43 +0700

    fix ui reading1 reading 2

--- src/components/Reading1.js
+++ src/components/Reading1.js
@@ -13,18 +13,9 @@ import * as examinationAPI from '../api/examination';
 export default function Reading1(props) {
     const [paragraph, setParagraph] = useState(props.exercise?.paragraph)
     const [question, setQuestion] = useState("");
-    const [answers, setAnswers] = useState([{}, {}, {}]);
+    const [answers, setAnswers] = useState([{ content: "False" }, { content: 'True' }]);
+    // const [answers, setAnswers] = useState([{}, {}, {}]);
     const [point, setPoint] = useState(null)
-
-    const onChangeAnswer = (event, index) => {
-        setAnswers(answers.map((value, idx) => {
-            if (idx === index) {
-                return { ...value, content: event.target.value }
-            }
-            return value;
-        }))
-
-    }
 
     const selectIsCorrect = (event) => {
         const isCorrectIndex = event.target.value;
@@ -63,22 +54,13 @@ export default function Reading1(props) {
             <Button variant="contained" onClick={onUpdateParagraph}>Update Paragraph</Button>
             <TextField label='Question' name='question' onChange={event => setQuestion(event.target.value)} />
             <RadioGroup
+                row
                 aria-labelledby="demo-row-radio-buttons-group-label"
                 name="radio-buttons-group"
                 onChange={selectIsCorrect}
             >
-                <div>
-                    <TextField label='answer' onChange={event => onChangeAnswer(event, 0)} control={<Radio />} style={{ width: 450 }} />
-                    <FormControlLabel value="0" control={<Radio />} />
-                </div>
-                <div>
-                    <TextField label='answer' onChange={event => onChangeAnswer(event, 1)} control={<Radio />} style={{ width: 450 }} />
-                    <FormControlLabel value="1" control={<Radio />} />
-                </div>
-                <div>
-                    <TextField label='answer' onChange={event => onChangeAnswer(event, 2)} control={<Radio />} style={{ width: 450 }} />
-                    <FormControlLabel value="2" control={<Radio />} />
-                </div>
+                <FormControlLabel label='True' value="1" control={<Radio />} />
+                <FormControlLabel label='False' value="0" control={<Radio />} />
             </RadioGroup>
             <TextField type='number' label='Point' name='point' onChange={event => setPoint(event.target.value)} />
             <Button variant="contained" onClick={onSubmit}>Add</Button>
