01b3feefc52d0b7126237e195a8d2ec5d2f18c85
Le Phu Quoc <tygovip119@gmail.com>
2023-10-17 00:56:02 +0700

    config.js

--- src/api/config.js
+++ src/api/config.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 // const URL = `http://localhost:3000/api/v1`
-const URL = `https://delf-practice.onrender.com/api/v1`
+const URL = `${process.env.REACT_APP_BACKEND_URL}/api/v1`
 export const api = axios.create({
     baseURL: URL
 })
